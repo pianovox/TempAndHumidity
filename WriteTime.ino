3b27800e7a3799931d74e041a59f500056adbbdf
pianovox <contact@doghousenyc.com>
2015-01-16 09:03:09 -0500

    clean up

--- WriteTime.ino
+++ WriteTime.ino
@@ -14,7 +14,7 @@ unsigned long convertToHours(unsigned long ms) {  //Hours
 }
 unsigned long convertToMinutes(unsigned long ms) {  //Minutes
   unsigned long seconds = ms/1000;
-  unsigned long minutes = seconds /60; 
+  unsigned long minutes = seconds / 60; 
   unsigned long hours = minutes/60;
   return (int) minutes;
 }
